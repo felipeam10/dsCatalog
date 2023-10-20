3378f2e8edfb05cca0c35ffb6579ede0adeecfa3
felipeam10 <felipeam10@hotmail.com>
2023-10-20 14:32:39 -0300

    V1.9 - Category Resource

---
+++ src/main/java/com/felipe/dsCatalog/resources/CategoryResources.java
@@ -0,0 +1,26 @@
+package com.felipe.dsCatalog.resources;
+
+import com.felipe.dsCatalog.entities.Category;
+import com.felipe.dsCatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+
+    @Autowired
+    private CategoryService service;
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+}
