7ee83a18306f737fa1faa783aa1c6f6996aa0346
felipeam10 <felipeam10@hotmail.com>
2023-10-20 14:34:05 -0300

    V1.11 - H2 database e camadas

---
+++ src/main/java/com/felipe/dsCatalog/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.felipe.dsCatalog.services;
+
+import com.felipe.dsCatalog.entities.Category;
+import com.felipe.dsCatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+}
