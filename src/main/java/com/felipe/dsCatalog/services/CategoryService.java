8e7d7033cad5c3681463b0d365cf74f65a6f08e3
felipeam10 <felipeam10@hotmail.com>
2023-10-22 00:48:06 -0300

    V1.16 - get category id

--- src/main/java/com/felipe/dsCatalog/services/CategoryService.java
+++ src/main/java/com/felipe/dsCatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.felipe.dsCatalog.services;
 import com.felipe.dsCatalog.dto.CategoryDTO;
 import com.felipe.dsCatalog.entities.Category;
 import com.felipe.dsCatalog.repositories.CategoryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -19,6 +21,13 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+       Optional<Category> obj = repository.findById(id);
+       Category entity = obj.get();
+       return new CategoryDTO(entity);
     }
 
 }
