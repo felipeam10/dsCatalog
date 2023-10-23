d75687ccca101a1445509e0dc768f19bd153763d
felipeam10 <felipeam10@hotmail.com>
2023-10-23 01:39:09 -0300

    V1.20 - DELETE category

--- src/main/java/com/felipe/dsCatalog/services/CategoryService.java
+++ src/main/java/com/felipe/dsCatalog/services/CategoryService.java
@@ -3,9 +3,12 @@ package com.felipe.dsCatalog.services;
 import com.felipe.dsCatalog.dto.CategoryDTO;
 import com.felipe.dsCatalog.entities.Category;
 import com.felipe.dsCatalog.repositories.CategoryRepository;
+import com.felipe.dsCatalog.services.exceptions.DataBaseException;
 import com.felipe.dsCatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -47,6 +50,19 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (jakarta.persistence.EntityNotFoundException e) {
             throw new EntityNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Falha de integridade referencial");
         }
     }
 }
