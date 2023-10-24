fdcf8301b58c00a43570df6116d592a2aa5cddae
felipeam10 <felipeam10@hotmail.com>
2023-10-24 16:05:37 -0300

    V1.25 - ProductDTO

--- src/main/java/com/felipe/dsCatalog/dto/ProductDTO.java
+++ src/main/java/com/felipe/dsCatalog/dto/ProductDTO.java
@@ -1,2 +1,83 @@
-package com.felipe.dsCatalog.dto;public class ProductDTO {
+package com.felipe.dsCatalog.dto;
+
+import com.felipe.dsCatalog.entities.Category;
+import com.felipe.dsCatalog.entities.Product;
+import jakarta.validation.constraints.*;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ProductDTO {
+
+    private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 03 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String name;
+    @Size(min = 10, message = "Descricao precisa ter no minimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String description;
+    @NotNull(message = "Campo requerido")
+    @Positive(message = "O preco deve ser positivo")
+    private Double price;
+    private String imgUrl;
+
+    private Instant date;
+
+    @NotEmpty(message = "Deve ter no mínimo uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO(){}
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+        date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categories) {
+        this(entity);
+        categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
 }
