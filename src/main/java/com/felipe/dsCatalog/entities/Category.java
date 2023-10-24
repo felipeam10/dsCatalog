71deb718c10e48003b4d8e461c20e24b395ebe5d
felipeam10 <felipeam10@hotmail.com>
2023-10-24 09:08:47 -0300

    V1.21 - Auditing data

--- src/main/java/com/felipe/dsCatalog/entities/Category.java
+++ src/main/java/com/felipe/dsCatalog/entities/Category.java
@@ -2,6 +2,7 @@ package com.felipe.dsCatalog.entities;
 
 import jakarta.persistence.*;
 
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -11,6 +12,12 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Category(Long id, String name) {
         this.id = id;
@@ -33,6 +40,24 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
