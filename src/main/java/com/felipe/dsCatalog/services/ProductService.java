189141d0c0c724ee02dd564da46de83f31dcdffb
felipeam10 <felipeam10@hotmail.com>
2023-10-25 02:39:32 -0300

    V1.27 - Product insert, update and delete

--- src/main/java/com/felipe/dsCatalog/services/ProductService.java
+++ src/main/java/com/felipe/dsCatalog/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.felipe.dsCatalog.services;
 
+import com.felipe.dsCatalog.dto.CategoryDTO;
 import com.felipe.dsCatalog.dto.ProductDTO;
+import com.felipe.dsCatalog.entities.Category;
 import com.felipe.dsCatalog.entities.Product;
+import com.felipe.dsCatalog.repositories.CategoryRepository;
 import com.felipe.dsCatalog.repositories.ProductRepository;
 import com.felipe.dsCatalog.services.exceptions.DataBaseException;
 import com.felipe.dsCatalog.services.exceptions.ResourceNotFoundException;
@@ -22,6 +25,9 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
         Page<Product> list = repository.findAll(pageRequest);
@@ -38,7 +44,7 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
-//        entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         entity =  repository.save(entity);
         return new ProductDTO(entity);
     }
@@ -46,8 +52,8 @@ public class ProductService {
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) {
         try {
-            Product entity = repository.getReferenceById(id);
-//            entity.setName(dto.getName());
+            Product entity = repository.getOne(id);
+            copyDtoToEntity(dto, entity);
             entity =  repository.save(entity);
             return new ProductDTO(entity);
         } catch (EntityNotFoundException e) {
@@ -65,6 +71,20 @@ public class ProductService {
         }
         catch (DataIntegrityViolationException e) {
             throw new DataBaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setDate(dto.getDate());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category cat = categoryRepository.getOne(catDto.getId());
+            entity.getCategories().add(cat);
         }
     }
 }
