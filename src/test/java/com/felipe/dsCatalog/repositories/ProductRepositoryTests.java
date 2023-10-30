890ac9d0590f88a1f6baa30bda20110039cc819c
felipeam10 <felipeam10@hotmail.com>
2023-10-30 11:22:06 -0300

    V2.17 - first repository test

---
+++ src/test/java/com/felipe/dsCatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,27 @@
+package com.felipe.dsCatalog.repositories;
+
+import com.felipe.dsCatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteOjectWhenIdExist() {
+
+        long existingId = 1L;
+
+        repository.deleteById(existingId);
+
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+}
