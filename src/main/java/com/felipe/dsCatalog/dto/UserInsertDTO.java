2e1fdbabcb248c5eb93a72d6d194a0f16eda577b
felipeam10 <felipeam10@hotmail.com>
2023-11-28 16:56:11 -0300

    v4.19 - Caso de uso signup, quando o usuario cria seu login

--- src/main/java/com/felipe/dsCatalog/dto/UserInsertDTO.java
+++ src/main/java/com/felipe/dsCatalog/dto/UserInsertDTO.java
@@ -1,11 +1,17 @@
 package com.felipe.dsCatalog.dto;
 
 import com.felipe.dsCatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 /* Esta anotation vai verificar se o email inserido já existe no banco de dados */
 @UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 8, message = "Deve ter no mínimo 8 caracteres")
+    /*@Pattern(regexp = ) pode tbm utilizar regex para validação de senhas */
     private String password;
 
     public UserInsertDTO(){
--- src/main/java/com/felipe/dsCatalog/repositories/RoleRepository.java
+++ src/main/java/com/felipe/dsCatalog/repositories/RoleRepository.java
@@ -4,4 +4,7 @@ import com.felipe.dsCatalog.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByAuthority(String authority);
+
 }
